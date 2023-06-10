f2a55683959e427d0c7d149953b65a02f7533587
Filip <filip.tokar@seznam.cz>
2023-06-10 21:35:04 +0200

    TourInfo + working carousel

--- src/components/MekongTour.jsx
+++ src/components/MekongTour.jsx
@@ -1,42 +1,12 @@
 import React from "react";
-import mainIMg from "../images/Component 1.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
-import arrowBtn from "../images/Component 2.png";
+import TourSocials from "./TourSocials"
+import TourInfo from "./TourInfo";
 
 function MekongTour() {
   return (
     <section id="mekong-tour">
-      <div className="tour__socials">
-        <figure className="img__wrapper">
-          <img src={mainIMg} />
-        </figure>
-
-        <div className="window__info">
-          
-            <p className="window__info--para">2 Days</p>
-            <div className="window__divider"></div>
-            <p className="window__info--price">$166 per person</p>
-          
-        </div>
-
-        <div className="social__list">
-          <a href="#" className="social__link social__link--hover">
-            <FontAwesomeIcon icon={faFacebookF} />
-          </a>
-          <a href="#" className="social__link social__link--hover">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-        </div>
-
-        <div className="more-trips">
-          <a href="" className="more-trips__link">
-            <img className="more-trips__wrapper" src={arrowBtn} />
-          </a>
-          <p className="more-tips__para">More Short Trips</p>
-        </div>
-      </div>
+      <TourSocials/>
+      <TourInfo/>
     </section>
   );
 }
---
+++ src/components/TourInfo.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import grid from "../images/Repeat Grid 11.png";
+import carouselImg1 from "../images/vietnam_foto.png";
+import carouselImg2 from "../images/shutterstock_1343264873-750x469.png";
+import carouselImg3 from "../images/Vietnam-1400x500-1080x500.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+function TourInfo() {
+  const [sliderRef, setSliderRef] = useState(null);
+
+  const sliderSettings = {
+    arrows: false,
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    infinite: false,
+    
+  };
+
+  return (
+    <div className="tour__info">
+      <div className="tour__info--text">
+        <h1 className="tour__info--heading">Mekong Delta Tour,</h1>
+        <span className="green">Vietnam</span>
+        <div className="divider__vertical"></div>
+        <div className="grid">
+          <img src={grid} alt="" />
+        </div>
+        <h2 className="tour__info--subheading">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo
+        </h2>
+        <button className="tour__info--btn">Book Now</button>
+      </div>
+
+      <button className="slider__btn--text" onClick={sliderRef?.slickNext}>
+            See more photos
+          </button>
+          <button className="slider__btn--arrow" onClick={sliderRef?.slickNext}>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+      <div className="slider__content">
+        
+          
+        
+        <Slider ref={setSliderRef} {...sliderSettings}>
+          <div>
+            <img className="slide" src={carouselImg1} alt="" />
+          </div>
+          <div>
+            <img className="slide" src={carouselImg2} alt="" />
+          </div>
+          <div>
+            <img className="slide" src={carouselImg3} alt="" />
+          </div>
+          <div>
+            <img className="slide" src={carouselImg1} alt="" />
+          </div>
+          <div>
+            <img className="slide" src={carouselImg2} alt="" />
+          </div>
+          <div>
+            <img className="slide" src={carouselImg3} alt="" />
+          </div>
+          <div>
+            <img className="slide" src={carouselImg1} alt="" />
+          </div>
+          <div>
+            <img className="slide" src={carouselImg2} alt="" />
+          </div>
+          <div>
+            <img className="slide" src={carouselImg3} alt="" />
+          </div>
+        </Slider>
+
+      </div>
+    </div>
+  );
+}
+
+export default TourInfo;
